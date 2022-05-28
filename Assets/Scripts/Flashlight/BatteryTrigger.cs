cb4870d6ac979df759eaff108df8a73d64001da4
EiguliteSupport <78115160+EiguliteSupport@users.noreply.github.com>
2022-05-28 17:43:22 +0300

    Flashlight battery runs out after 5 seconds, but the battery yet doesn't work, thought it should

--- Assets/Scripts/Flashlight/BatteryTrigger.cs
+++ Assets/Scripts/Flashlight/BatteryTrigger.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BatteryTrigger : MonoBehaviour
 {
+    public FlashlightToggle FlashLightScript;
+
     void OnTriggerEnter(Collider other)
     {
+        FlashLightScript.ResetTimer();
         gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Flashlight/FlashlightToggle.cs
+++ Assets/Scripts/Flashlight/FlashlightToggle.cs
@@ -3,24 +3,53 @@ using UnityEngine;
 public class FlashlightToggle : MonoBehaviour
 {
     public Light flashLight;
-    private bool isFlashLightActive;
+    bool isFlashLightActive;
+
+    private float currentTime = 0f;
+    private float startingTime = 5f;
 
     void Start()
     {
         flashLight.enabled = false;
+
+        currentTime = startingTime;
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F) && isFlashLightActive == false)
+        // Turns on the flashlight
+        if (Input.GetKeyDown(KeyCode.F) && isFlashLightActive == false)
         {
             flashLight.enabled = true;
             isFlashLightActive = true;
         }
-        else if(Input.GetKeyDown(KeyCode.F) && isFlashLightActive == true)
+
+        // Turns off the flashlight
+        else if (Input.GetKeyDown(KeyCode.F) && isFlashLightActive == true)
         {
             flashLight.enabled = false;
             isFlashLightActive = false;
         }
+
+        Timer();
+    }
+    
+    void Timer()
+    {
+        if (isFlashLightActive == true)
+        {
+            currentTime -= Time.deltaTime;
+            print(currentTime);
+
+            if(currentTime <= 0)
+            {
+                flashLight.enabled = false;
+            }
+        }
+    }
+
+    public void ResetTimer()
+    {
+        currentTime = startingTime;
     }
 }
