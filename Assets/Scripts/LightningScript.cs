b73857e1e7a545621b01a7cf8427c32e347d9734
werwolf <eric.hahn.business@gmail.com>
2022-05-28 17:02:18 +0200

    add random lightning for dark mood

---
+++ Assets/Scripts/LightningScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningScript : MonoBehaviour
+{
+    Light lightComponent;
+    float minIntensity = 5;
+    float maxIntensity = 20;
+    float nextLightning = 0;
+    float lightningDuration = 1;
+    float endOfLightning = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightComponent = this.GetComponent<Light>();
+        nextLightning = Time.time + 3;
+        endOfLightning = nextLightning + lightningDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextLightning){
+            lightComponent.intensity = Random.Range(minIntensity,maxIntensity);
+            if(Time.time > endOfLightning){
+                nextLightning = Time.time + Random.Range(1,6);
+                endOfLightning = nextLightning + lightningDuration;
+            }
+        }
+        else{
+            lightComponent.intensity = 0;
+        } 
+    }
+}
