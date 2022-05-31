e4370f96b76e0e6d85c3a4a0a0f9479a45485a5c
werwolf <eric.hahn.business@gmail.com>
2022-05-31 22:05:52 +0200

    ring of fire

---
+++ Assets/Scripts/FloorFire.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorFire : MonoBehaviour
+{
+    [SerializeField]
+    Transform center;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(center == null) return;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(center == null) return;
+        Vector3 position = transform.position;
+        Quaternion rotation = transform.rotation;
+        position.x = center.position.x + 4*Mathf.Sin(Time.time*10);
+        position.z = center.position.z + 4*Mathf.Cos(Time.time*10);
+        transform.SetPositionAndRotation(position,rotation);
+
+    }
+}
