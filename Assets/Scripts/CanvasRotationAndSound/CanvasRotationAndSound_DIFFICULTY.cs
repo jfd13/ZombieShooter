655fa413f59d6125ce113a5ae4e961c7f89ef070
jfd <patrik.toplak@gmail.com>
2022-06-06 20:07:57 +0200

    Added on click sounds and applied them to buttons

---
+++ Assets/Scripts/CanvasRotationAndSound/CanvasRotationAndSound_DIFFICULTY.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasRotationAndSound_DIFFICULTY : MonoBehaviour
+{
+    public Transform canvas;
+    public Transform player;
+    public AudioClip[] audioClips = new AudioClip[0];
+    public AudioSource onClickAudioSource;
+
+
+    int random;
+
+    public void Update()
+    {
+        TurnToPlayer();
+    }
+
+    public void TurnToPlayer()
+    {
+        Vector3 lookPos = player.position - canvas.transform.position;
+        Quaternion lookRot = Quaternion.LookRotation(lookPos, Vector3.up);
+        float eulerY = lookRot.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, eulerY - 180, 0);
+        canvas.transform.rotation = rotation;
+    }
+
+    public void OnClickSound()
+    {
+        random = Random.Range(0, 12);
+
+        for (int i = random; i < random + 1; i++)
+        {
+            onClickAudioSource.clip = audioClips[random];
+
+            if (onClickAudioSource.clip == null)
+            {
+                return;
+            }
+            else
+            {
+                onClickAudioSource.Play();
+            }
+        }
+    }
+}
