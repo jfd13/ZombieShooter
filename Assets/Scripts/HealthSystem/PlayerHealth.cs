b5d5eeb0266b7a0037712f4d1f1e8a07bee6e0d5
jfd <patrik.toplak@gmail.com>
2022-06-07 16:24:09 +0200

    Added scripts and assets for aid kit

---
+++ Assets/Scripts/HealthSystem/PlayerHealth.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth;
+    float currentHealth;
+    float randomModifier;
+    public float minRandomModifier;
+    public float maxRandomModifier;
+
+    public void Start()
+    {
+        SetVariables();
+    }
+
+    public void Health(float healthSubstraction)
+    {
+        randomModifier = Random.Range(minRandomModifier, maxRandomModifier);
+
+        currentHealth -= healthSubstraction * randomModifier;
+
+        if (currentHealth > 0)
+        {
+            return;
+        }
+        else if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        //Play sound
+        //Screen goes black
+        //Game over screen
+    }
+
+    public void SetVariables()
+    {
+        currentHealth = maxHealth;
+    }
+}
